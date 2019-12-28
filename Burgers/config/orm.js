7f3f5b37ec9a9d314eb6f2d561b829035a3c08a3
Aiysha Sadana <aiyshasadana@gmail.com>
2019-12-27 20:22:32 -0500

    pushing changes

---
+++ Burgers/config/orm.js
@@ -0,0 +1,10 @@
+var connection = require('./connection');
+
+var orm = {
+    all:function(tableInput, cb){
+        connection.query('SELECT * FROM'+tableInput+';', function(err, result){
+            if(err) throw err;
+            cb(result)
+        })
+    }
+}
